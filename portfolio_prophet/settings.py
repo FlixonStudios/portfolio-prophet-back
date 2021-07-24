26f9455ecb5dafc5f67d3e822b464a2b174e1927
chiakaiming <chiakaiming@hotmail.com>
2021-07-24 15:10:10 +0800

    added react stuff

--- portfolio_prophet/settings.py
+++ portfolio_prophet/settings.py
@@ -114,7 +114,7 @@ ROOT_URLCONF = 'portfolio_prophet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "build")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
