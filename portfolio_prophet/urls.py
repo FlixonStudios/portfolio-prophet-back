06cb79dcc4b7807808131f720a4de7b5e2b89989
chiakaiming <chiakaiming@hotmail.com>
2021-07-25 21:12:17 +0800

    static path

--- portfolio_prophet/urls.py
+++ portfolio_prophet/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -30,4 +32,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('accounts/', include('accounts.urls')),
     re_path('(^(?!(api|admin)).*$)', TemplateView.as_view(template_name='index.html')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
