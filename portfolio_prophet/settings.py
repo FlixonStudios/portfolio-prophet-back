8b9c103d86d58b046555557f1f629d83f1728990
chiakaiming <chiakaiming@hotmail.com>
2021-07-25 21:19:53 +0800

    static

--- portfolio_prophet/settings.py
+++ portfolio_prophet/settings.py
@@ -190,9 +190,9 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'deployment', 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(APP_DIR, 'static')),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static')),
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
